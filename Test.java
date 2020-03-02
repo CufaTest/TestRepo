24960e30c4840ebbbddf5a8b43ae8b45894b7dc3
arjanitisufii <arianitisufi00@gmail.com>
2020-03-02 23:38:11 +0100

    Added name

--- Test.java
+++ Test.java
@@ -3,8 +3,9 @@ public class Test {
 	public static void main (String [] args) {
 		
 		System.out.println("First git pull/push test from 2 diffrenet pc");
-		//Changes made from hp !
-		
+			//Changes made from hp !
+		String name = "cufa";
+		int password = 123;	
 		
 	}
 }

5082600e98abeaca50d9276b6e15293b99f14d94
CufaTest <cufaelm@gmail.com>
2020-03-02 23:09:32 +0100

    Changes from newBranch

--- Test.java
+++ Test.java
@@ -3,8 +3,8 @@ public class Test {
 	public static void main (String [] args) {
 		
 		System.out.println("First git pull/push test from 2 diffrenet pc");
-		//Changes made from hp !
-		
+				//Changes made from hp !
+			//THIS IS FROM NEWBRANCH 
 		
 	}
 }
